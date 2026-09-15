f6a77b804b8d4c71dc67df885449492eedddd8aa
sgurung183 <gurungsushant30@gmail.com>
2026-09-14 21:20:08 -0700

    initialize sevice

---
+++ src/main/java/com/SplitIt/group_service/GroupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.SplitIt.group_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SplitIt/group_service/GroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SplitIt.group_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
